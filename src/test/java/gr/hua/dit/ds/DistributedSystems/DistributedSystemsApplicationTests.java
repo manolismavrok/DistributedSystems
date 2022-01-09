1dd69e350fccd5d6308cf3a60c807b559f105d2c
manolismavrok <manolismavrok@gmail.com>
2022-01-09 23:23:43 +0200

    Adjusting application.properties

--- src/test/java/gr/hua/dit/ds/DistributedSystems/DistributedSystemsApplicationTests.java
+++ src/test/java/gr/hua/dit/ds/DistributedSystems/DistributedSystemsApplicationTests.java
@@ -3,7 +3,7 @@ package gr.hua.dit.ds.DistributedSystems;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DistributedSystemsApplicationTests {
 
 	@Test
